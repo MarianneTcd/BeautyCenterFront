ac9204ebe0fbeb5cb40fd0adce0fe5c114b8c573
AlexandreGross <gross.alexandre@hotmail.fr>
2019-04-19 11:44:57 +0200

    index + createSalonComponent

---
+++ src/app/create-salon/create-salon.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-salon',
+  templateUrl: './create-salon.component.html',
+  styleUrls: ['./create-salon.component.css']
+})
+export class CreateSalonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
