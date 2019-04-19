8ba9abf508df6cd2e22aa82b77cc7da146aa85f7
Jephy78 <jeanphilippelaine@outlook.fr>
2019-04-19 15:06:43 +0200

    liste salons v2

--- src/app/liste-salons/liste-salons.component.ts
+++ src/app/liste-salons/liste-salons.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-liste-salons',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./liste-salons.component.css']
 })
 export class ListeSalonsComponent implements OnInit {
-
   constructor() { }
-
+  ngOnInit() {}
+  /*constructor(private http:Http) { }
+  
+  data;
   ngOnInit() {
-  }
+    this.http.get('http://localhost:8080/users')
+    .subscribe(
+      response=>{
+        console.log(response.json());
+        this.data = response.json();
+      }
+    );
+}*/
 
 }
