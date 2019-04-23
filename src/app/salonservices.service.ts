6cbb01fda1bc71b7622c2ce43f1948db8d89230a
MartinLenglet <martinlenglet@gmail.com>
2019-04-23 14:51:08 +0200

    page salon

--- src/app/salonservices.service.ts
+++ src/app/salonservices.service.ts
@@ -5,8 +5,9 @@ import { Injectable } from '@angular/core';
 })
 export class SalonservicesService {
 
-  constructor() { }
-
+  constructor(
+  ) { }
+id;
   dateToday(){
     return new Date();
   }
