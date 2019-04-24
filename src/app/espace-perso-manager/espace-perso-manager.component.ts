7ae0f8a0c8eebc4682b78971f33414d37b6e141b
AlexandreGross <gross.alexandre@hotmail.fr>
2019-04-24 15:44:12 +0200

    maj manager

--- src/app/espace-perso-manager/espace-perso-manager.component.ts
+++ src/app/espace-perso-manager/espace-perso-manager.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ServiceStockageService } from '../service-stockage.service';
 import { Http } from '@angular/http';
 import { Salon } from '../model/Salon';
+import { identifierModuleUrl } from '@angular/compiler';
+import { Prestation } from '../model/Prestation';
+import { Event } from '../model/Event';
 
 @Component({
   selector: 'app-espace-perso-manager',
@@ -9,12 +13,23 @@ import { Salon } from '../model/Salon';
 })
 export class EspacePersoManagerComponent implements OnInit {
 
-  constructor(private http: Http) { }
+  constructor(private http: Http, private stockageService: ServiceStockageService) { }
 
+  //RECUPERATION DE L'ID DU MANAGER
+  idMan = this.stockageService.id;
+  res;
   ngOnInit() {
     this.chargeListeSalons();
+    this.http.get('http://localhost:8080/users/' + this.idMan)
+      .subscribe(
+        response => {
+          console.log(response.json());
+          this.res = response.json();
+        }
+      )
   }
 
+  // RECUPERATION DE LA LISTE DES SALONS DU MANAGER
   dataSalons;
   chargeListeSalons() {
     this.http.get('http://localhost:8080/salons').subscribe(response => {
@@ -22,6 +37,7 @@ export class EspacePersoManagerComponent implements OnInit {
     });
   }
 
+  // CREATION D'UN SALON POUR LE MANAGER
   creation = false;
   salon: Salon = new Salon();
   createSalon() {
@@ -33,4 +49,66 @@ export class EspacePersoManagerComponent implements OnInit {
       console.log(err);
     });
   }
+
+  //SUPPRESION D'UN SALON ET EVENT DU MANAGER
+  suppression = false;
+  supSalon(id) {
+    this.http.delete('http://localhost:8080/events/salon/' + id).subscribe(eventD => {
+      console.log(eventD);
+    }, err => {
+      console.log(err);
+    });
+    this.http.delete('http://localhost:8080/salons/' + id).subscribe(salonD => {
+      console.log(salonD);
+      this.suppression = true;
+      this.chargeListeSalons();
+    }, err => {
+      console.log(err);
+    });
+  }
+
+  //AFFICHER / CACHER DES INFOS
+  show = false;
+  affCach() {
+    this.show = !this.show;
+  }
+
+  // RECUPERATION DE LA LISTE DES PRESTATIONS DU SALON
+  dataPresta;
+  chargeListePresta(id) {
+    this.http.get('http://localhost:8080/events/salon/' + id).subscribe(response => {
+      this.dataPresta = response.json();
+    });
+  }
+
+  // AJOUT D'UNE PRESTATION AU SALON
+  prestation: Prestation = new Prestation();
+  pass = false;
+  createPrestation() {
+    if (this.prestation.titre == null || 0 || this.prestation.duree == null || 0 || this.prestation.nbPersonnel == null || 0) {
+      this.pass = true;
+    } else {
+      this.pass = false;
+      this.http.post('http://localhost:8080/prestations', this.prestation).subscribe(prestationData => {
+        console.log(prestationData);
+        this.affCach();
+      }, err => {
+        console.log(err);
+      });
+    }
+  }
+
+  //CREATION PRESTATION ET EVENT
+  event: Event = new Event();
+  createEvent() {
+    this.createPrestation();
+    this.http.post('http://localhost:8080/events', this.event).subscribe(eventData => {
+      console.log(eventData);
+      this.affCach();
+    }, err => {
+      console.log(err);
+    });
+  }
+
+
 }
