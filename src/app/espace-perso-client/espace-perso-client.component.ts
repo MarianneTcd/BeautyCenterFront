7e0af2fafff74e9c730ef5228a4ff61d5184f94f
OceaneYing <yueh.music@gmail.com>
2019-04-25 12:17:55 +0200

    modif modif profile espace client

--- src/app/espace-perso-client/espace-perso-client.component.ts
+++ src/app/espace-perso-client/espace-perso-client.component.ts
@@ -11,7 +11,14 @@ import { userInfo } from 'os';
 })
 export class EspacePersoClientComponent implements OnInit {
   
-  user: User= new User();
+  user1: User = new User();
+  user2: User = new User();
+  user3: User = new User();
+  u: User = new User();
+  mdpA;
+  mdpN1;
+  passS=false;
+  passE=false;
 
   constructor(private http: Http, private stockageService: ServiceStockageService) { }
   id = this.stockageService.id;
@@ -20,6 +27,7 @@ export class EspacePersoClientComponent implements OnInit {
   prenom;
   mail;
   mdp;
+  access;
 
   ngOnInit() {
     this.http.get('http://localhost:8080/users/' + this.id)
@@ -30,13 +38,116 @@ export class EspacePersoClientComponent implements OnInit {
         this.nom = this.res.nom;
         this.prenom = this.res.prenom;
         this.mail = this.res.mail;
-        this.mdp = this.res.mdp
+        this.mdp = this.res.mdp;
+        this.access = this.res.access;
       } )
 
   }
+  
+  modifNom(){
+    this.user1.id = this.res.id;
+    this.user1.prenom = this.res.prenom;
+    this.user1.mail = this.res.mail;
+    this.user1.mdp = this.res.mdp;
+    this.user1.access = this.res.access;
 
-  modifprof() {
+    this.http.put('http://localhost:8080/user/' + this.id, this.user1).
+    subscribe(userData => {
+      console.log(userData);
+    }, err => {
+      console.log(err);
+    });
+
+    this.http.get('http://localhost:8080/users/' + this.id)
+    .subscribe(response => { 
+        console.log(response.json()); 
+        this.res= response.json();
+        this.nom = this.res.nom;
+    });
+  }
+
+  modifPrenom(){
+    this.user2.id = this.res.id;
+    this.user2.nom = this.res.nom;
+    this.user2.mail = this.res.mail;
+    this.user2.mdp = this.res.mdp;
+    this.user2.access = this.res.access;
+
+    this.http.put('http://localhost:8080/user/' + this.id, this.user2).
+    subscribe(userData => {
+      console.log(userData);
+    }, err => {
+      console.log(err);
+    });
+
+    this.http.get('http://localhost:8080/users/' + this.id)
+    .subscribe(response => { 
+        console.log(response.json()); 
+        this.res= response.json();
+        this.prenom = this.res.prenom;
+    });
+  }
+
+  modifMail(){
+    this.user3.id = this.res.id;
+    this.user3.nom = this.res.nom;
+    this.user3.prenom = this.res.prenom;
+    this.user3.mdp = this.res.mdp;
+    this.user3.access = this.res.access;
+
+    this.http.put('http://localhost:8080/user/' + this.id, this.user3).
+    subscribe(userData => {
+      console.log(userData);
+    }, err => {
+      console.log(err);
+    });
+
+    this.http.get('http://localhost:8080/users/' + this.id)
+    .subscribe(response => { 
+        console.log(response.json()); 
+        this.res= response.json();
+        this.mail = this.res.mail;
+    });
+  }
+
+  modifMdp() {
+
+    this.u.id = this.res.id;
+    this.u.nom = this.res.nom;
+    this.u.prenom = this.res.prenom;
+    this.u.mail = this.res.mail;
+    this.u.access = this.res.access;
+
+    if (this.mdpA === this.res.mdp && this.mdpN1 === this.u.mdp){
+      this.http.put('http://localhost:8080/user/' + this.id, this.u).subscribe(userData=>{
+        console.log(userData);
+      }, err => {
+        console.log(err);
+      });
+      console.log('la modification de mot de passe a bien été prise en compte');
+      this.passS = true;
+      this.passE = false;
+    }else {
+      console.log('la modification de mot de passe est echouée');
+      this.passS = false;
+      this.passE = true;
+    }
+  }
+
+  /*
+  modifProf() {
+   
+    this.user.id = this.res.id;
+    this.user.mdp = this.res.mdp;
+    this.user.access = this.res.access;
+
+    this.http.put('http://localhost:8080/user/' + this.id, this.user).subscribe(userData=>{
+      console.log(userData);
+    }, err => {
+      console.log(err);
+    });
 
   }
+*/
 
 }
