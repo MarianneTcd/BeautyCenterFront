469bb941aa03649c801cee2519d811f5e9adc92d
OceaneYing <yueh.music@gmail.com>
2019-04-26 11:42:39 +0200

    123

--- src/app/espace-admin/espace-admin.component.ts
+++ src/app/espace-admin/espace-admin.component.ts
@@ -12,27 +12,37 @@ export class EspaceAdminComponent implements OnInit {
 
   constructor(private http: Http, private stockageService: ServiceStockageService) { }
 
+  //récupérer l'ID et stocker les info de l'administrateur sur la session
   id = this.stockageService.id;
   resUser;
-  nomA; //déclarer les info d'administrateur sur la session
+  nomA; 
   prenomA;
   mailA;
   mdpA;
   accessA;
-  managerModif //déclarer les info de manager que l'administrateur souhaite modifier
+
+  //stocker les info du manager que l'administrateur souhaite modifier
+  managerModif 
   nomM;
   prenomM;
   mailM;
   mdpM;
   accessM;
-  user1: User = new User();
-  user2: User = new User();
-  user3: User = new User();
-  u: User = new User();
-  passS=false;
-  passE=false;
-  manager; //déclarer le variable qui chargera la liste de tous les managers
-  
+
+  //variable qui chargera la liste de tous les managers
+  manager;  
+
+  //variable qui charge le mail de manager crée qui est utilisé pour envoyer un mail
+  mail; 
+
+  //variable pour switcher entre le panneau d'ajout et de modification de manager
+  show = true; 
+
+  //stocker les info du manager à ajouter;
+  man: User = new User();
+
+
+
   ngOnInit() {
     this.http.get('http://localhost:8080/users/' + this.id)
     .subscribe(
@@ -65,10 +75,27 @@ export class EspaceAdminComponent implements OnInit {
         this.mailM = this.managerModif.mail;
         this.mdpM = this.managerModif.mdp;
       } )
-    
-
+    this.show = false;
   }
 
+  showAjout(){
+    this.show = true;
+  }
 
+  createManager(){
+    this.man.access = 3;
+    this.http.post('http://localhost:8080/users', this.man).subscribe(userData=>{
+      console.log(userData);      
+    }, err => {
+      console.log(err);
+    });
 
+    this.http.get('http://localhost:8080/users/managers')
+    .subscribe(
+      response => { 
+        console.log(response.json()); 
+        this.manager= response.json();
+      } )
+  }
+  
 }
