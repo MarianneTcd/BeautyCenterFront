dd921f0126fd63755a7c5a83a9f05a653603bbdb
AlexandreGross <gross.alexandre@hotmail.fr>
2019-04-26 20:46:41 +0200

    maj admin

--- src/app/material.ts
+++ src/app/material.ts
@@ -5,10 +5,13 @@ import {MatInputModule} from '@angular/material/input';
 import {MatDialogModule} from '@angular/material/dialog';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatListModule} from '@angular/material/list'; 
 
 @NgModule({
-    imports: [MatButtonModule, MatCheckboxModule, MatAutocompleteModule, MatInputModule, MatDialogModule, MatCardModule, MatButtonToggleModule],
-    exports: [MatButtonModule, MatCheckboxModule, MatAutocompleteModule, MatInputModule, MatDialogModule, MatCardModule, MatButtonToggleModule],
+    imports: [MatButtonModule, MatCheckboxModule, MatAutocompleteModule,
+    MatInputModule, MatDialogModule, MatCardModule, MatButtonToggleModule, MatListModule],
+    exports: [MatButtonModule, MatCheckboxModule, MatAutocompleteModule,
+        MatInputModule, MatDialogModule, MatCardModule, MatButtonToggleModule, MatListModule],
     })
 
 export class MaterialModule { }
